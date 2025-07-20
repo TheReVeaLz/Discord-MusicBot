6f5ae7c395b07124912b60279bf59fbd17d85923
TheReVeaLz <70447633+TheReVeaLz@users.noreply.github.com>
2025-07-20 20:46:02 +0700

    fix: context menu play song

--- commands/context/play.js
+++ commands/context/play.js
@@ -2,6 +2,12 @@ const { ContextMenuCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder, Colors, ApplicationIntegrationType } = require("discord.js");
 const escapeMarkdown = require("discord.js").escapeMarkdown;
 
+function extractURLs(text = "") {
+  const regex = /https?:\/\/(?:www\.)?[a-zA-Z0-9-%.]+(?:\.[a-zA-Z]{2,6})(?:[\/?][^\s]*)?/g;
+  const urls = text.match(regex);
+  return urls?.filter?.(url => new URL(url)) ?? undefined;
+}
+
 module.exports = {
   command: new ContextMenuCommandBuilder()
   .setName("Play Song")
@@ -14,168 +20,185 @@ module.exports = {
    * @param {import("discord.js").GuildContextMenuInteraction} interaction
    */
   run: async (client, interaction, options) => {
-    let channel = await client.getChannel(client, interaction);
-    if (!channel) {
-      return;
-    }
-
-    let node = await client.getLavalink(client);
-    if (!node) {
-      return interaction.reply({
-        embeds: [client.ErrorEmbed("Lavalink node is not connected")],
-      });
-    }
-
-    let player = client.createPlayer(interaction.channel, channel);
-
-    if (player.state !== "CONNECTED") {
-      player.connect();
-    }
-
-    if (channel.type == "GUILD_STAGE_VOICE") {
-      setTimeout(() => {
-        if (interaction.guild.me.voice.suppress == true) {
-          try {
-            interaction.guild.me.voice.setSuppressed(false);
-          } catch (e) {
-            interaction.guild.me.voice.setRequestToSpeak(true);
+    try {
+      const channel = await client.getChannel(client, interaction);
+      if (!channel) {
+        return;
+      }
+  
+      const node = await client.getLavalink(client);
+      if (!node) {
+        return interaction.reply({
+          embeds: [client.ErrorEmbed("Lavalink node is not connected")],
+        });
+      }
+  
+      const player = client.createPlayer(interaction.channel, channel);
+  
+      if (player.state !== "CONNECTED") {
+        player.connect();
+      }
+  
+      if (channel.type == "GUILD_STAGE_VOICE") {
+        setTimeout(() => {
+          if (interaction.guild.me.voice.suppress == true) {
+            try {
+              interaction.guild.me.voice.setSuppressed(false);
+            } catch (e) {
+              interaction.guild.me.voice.setRequestToSpeak(true);
+            }
           }
-        }
-      }, 2000); // Need this because discord api is buggy asf, and without this the bot will not request to speak on a stage - Darren
-    }
-
-    const ret = await interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor(client.config.embedColor)
-          .setDescription(":mag_right: **Searching...**"),
-      ],
-      withResponse: true,
-    });
-
-    const query =
-      interaction.channel.messages.cache.get(interaction.targetId).content ??
-      (await interaction.channel.messages.fetch(interaction.targetId));
-    let res = await player.search(query, interaction.user).catch((err) => {
-      client.error(err);
-      return {
-        loadType: "LOAD_FAILED",
-      };
-    });
-
-    if (res.loadType === "LOAD_FAILED") {
-      if (!player.queue.current) {
-        player.destroy();
+        }, 2000); // Need this because discord api is buggy asf, and without this the bot will not request to speak on a stage - Darren
       }
-      await interaction
-        .editReply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor(Colors.Red)
-              .setDescription("There was an error while searching"),
-          ],
-        })
-        .catch(this.warn);
-    }
+  
+      const ret = await interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(client.config.embedColor)
+            .setDescription(":mag_right: **Searching...**"),
+        ],
+        withResponse: true,
+      });
+  
+      const msg = interaction.channel.messages.cache.get(interaction.targetId).content ?? (await interaction.channel.messages.fetch(interaction.targetId));
+      const query = extractURLs(msg)?.[0] ?? msg; 
+      const res = await player.search(query, interaction.user).catch((err) => {
+        client.error(err);
+        return {
+          loadType: "LOAD_FAILED",
+        };
+      });
 
-    if (res.loadType === "NO_MATCHES") {
-      if (!player.queue.current) {
-        player.destroy();
-      }
-      await interaction
-        .editReply({
-          embeds: [
-            new EmbedBuilder()
-              .setColor(Colors.Red)
-              .setDescription("No results were found"),
-          ],
-        })
-        .catch(this.warn);
-    }
+      switch (res.loadType) {
+        case "LOAD_FAILED":
+        case "error":
+          await interaction
+            .editReply({
+              embeds: [
+                new EmbedBuilder()
+                  .setColor(Colors.Red)
+                  .setDescription("There was an error while searching"),
+              ],
+            })
+            .catch(this.warn);
+          break;
 
-    if (res.loadType === "TRACK_LOADED" || res.loadType === "SEARCH_RESULT") {
-      player.queue.add(res.tracks[0]);
+        case "NO_MATCHES":
+        case "empty":
+        await interaction
+          .editReply({
+            embeds: [
+              new EmbedBuilder()
+                .setColor(Colors.Red)
+                .setDescription("No results were found"),
+            ],
+          })
+          .catch(this.warn);
+          break;
 
-      if (!player.playing && !player.paused && !player.queue.size) {
-        player.play();
-      }
-      var title = escapeMarkdown(res.tracks[0].title);
-      var title = title.replace(/\]/g, "");
-      var title = title.replace(/\[/g, "");
-      let addQueueEmbed = new EmbedBuilder()
-        .setColor(client.config.embedColor)
-        .setAuthor({ name: "Added to queue", iconURL: client.config.iconURL })
-        .setDescription(`[${title}](${res.tracks[0].uri})` || "No Title")
-        .setURL(res.tracks[0].uri)
-        .setThumbnail(res.tracks[0].displayThumbnail ? res.tracks[0].displayThumbnail("maxresdefault") : res.tracks[0].thumbnail)
-        .addFields(
-          {
-            name: "Added by",
-            value: `<@${interaction.user.id}>`,
-            inline: true,
-          },
-          {
-            name: "Duration",
-            value: res.tracks[0].isStream
-              ? `\`LIVE 🔴 \``
-              : `\`${client.ms(res.tracks[0].duration, {
+        case "TRACK_LOADED":
+        case "SEARCH_RESULT":
+        case "search":
+        case "track":
+          player.queue.add(res.tracks[0]);
+    
+          if (!player.playing && !player.paused && !player.queue.size) {
+            player.play();
+          }
+          var title = escapeMarkdown(res.tracks[0].info.title);
+          var title = title.replace(/\]/g, "");
+          var title = title.replace(/\[/g, "");
+          let addQueueEmbed = new EmbedBuilder()
+            .setColor(client.config.embedColor)
+            .setAuthor({ name: "Added to queue", iconURL: client.config.iconURL })
+            .setDescription(`[${title}](${res.tracks[0].info.uri})` || "No Title")
+            .setURL(res.tracks[0].info.uri)
+            .setThumbnail(res.tracks[0].info.displayThumbnail ? res.tracks[0].info.displayThumbnail("maxresdefault") : res.tracks[0].info.artworkUrl)
+            .addFields(
+              {
+                name: "Added by",
+                value: `<@${interaction.user.id}>`,
+                inline: true,
+              },
+              {
+                name: "Duration",
+                value: res.tracks[0].info.isStream
+                  ? `\`LIVE 🔴 \``
+                  : `\`${client.ms(res.tracks[0].info.duration, {
+                      colonNotation: true,
+                      secondsDecimalDigits: 0,
+                    })}\``,
+                inline: true,
+              }
+            );
+    
+          if (player.queue.totalSize > 1) {
+            addQueueEmbed.addFields({
+              name: "Position in queue",
+              value: `${player.queue.size}`,
+              inline: true,
+            });
+          }
+    
+          await interaction.editReply({ embeds: [addQueueEmbed] }).catch(this.warn);
+          break;
+
+        case "PLAYLIST_LOADED":
+        case "playlist":
+          player.queue.add(res.tracks);
+
+          if (
+            !player.playing &&
+            !player.paused &&
+            player.queue.tracks.length === res.tracks.length
+          ) {
+            player.play();
+          }
+    
+          let playlistEmbed = new EmbedBuilder()
+            .setColor(client.config.embedColor)
+            .setAuthor({
+              name: "Playlist added to queue",
+              iconURL: client.config.iconURL,
+            })
+            .setThumbnail(res.tracks[0].info.artworkUrl)
+            .setDescription(`[${res.playlist.name}](${query})`)
+            .addFields(
+              {
+                name: "Enqueued",
+                value: `\`${res.tracks.length}\` songs`,
+                inline: true,
+              },
+              {
+                name: "Playlist duration",
+                value: `\`${client.ms(res.playlist.duration, {
                   colonNotation: true,
                   secondsDecimalDigits: 0,
                 })}\``,
-            inline: true,
-          }
-        );
+                inline: true,
+              }
+            );
+    
+          await interaction.editReply({ embeds: [playlistEmbed] }).catch(this.warn);
+          break;
 
-      if (player.queue.totalSize > 1) {
-        addQueueEmbed.addFields({
-          name: "Position in queue",
-          value: `${player.queue.size}`,
-          inline: true,
-        });
+        default:
+          client.error(`Unknown type: ${res.loadType}`);
+          await interaction
+            .editReply({
+              embeds: [
+                new EmbedBuilder()
+                  .setColor(Colors.Red)
+                  .setDescription("There is something wrong"),
+              ],
+            })
+            .catch(this.warn);
+          break;
       }
 
-      await interaction.editReply({ embeds: [addQueueEmbed] }).catch(this.warn);
+      return ret;
+    } catch (err) {
+      client.error(err.message);
     }
-
-    if (res.loadType === "PLAYLIST_LOADED") {
-      player.queue.add(res.tracks);
-
-      if (
-        !player.playing &&
-        !player.paused &&
-        player.queue.totalSize === res.tracks.length
-      ) {
-        player.play();
-      }
-
-      let playlistEmbed = new EmbedBuilder()
-        .setColor(client.config.embedColor)
-        .setAuthor({
-          name: "Playlist added to queue",
-          iconURL: client.config.iconURL,
-        })
-        .setThumbnail(res.tracks[0].thumbnail)
-        .setDescription(`[${res.playlist.name}](${query})`)
-        .addFields(
-          {
-            name: "Enqueued",
-            value: `\`${res.tracks.length}\` songs`,
-            inline: true,
-          },
-          {
-            name: "Playlist duration",
-            value: `\`${client.ms(res.playlist.duration, {
-              colonNotation: true,
-              secondsDecimalDigits: 0,
-            })}\``,
-            inline: true,
-          }
-        );
-
-      await interaction.editReply({ embeds: [playlistEmbed] }).catch(this.warn);
-    }
-
-    if (ret) setTimeout(() => ret.delete().catch(this.warn), 20000);
-    return ret;
   },
 };
